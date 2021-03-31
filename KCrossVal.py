4869df44350fa7f59717e694c43941f5ea2712d9
FeLusiani <felusiani@gmail.com>
2021-03-31 23:54:07 +0200

    added docstrings

--- KCrossVal.py
+++ KCrossVal.py
@@ -10,6 +10,19 @@ import time
 
 # function that train the algorithm with the function passed
 def kFoldCross(trainCallback, predictCallback, X_dev, Y_dev, n_splits, err_func):
+    """Run training and validation using k-fold cross-validation.
+
+    Args:
+        trainCallback (callable object): function to fit model to the data.
+        predictCallback (callable object): function to produce model prediction
+        X_dev (np.ndarray): mode input data
+        Y_dev (np.ndarray): model target data
+        n_splits (int): n. of folds
+        err_func (callable object): error measurement function
+
+    Returns:
+        results (tuple): `mean_ValError, std_ValError, mean_TrError, seconds`
+    """
     kf = KFold(n_splits=n_splits)
     ValError = []
     TrError = []
@@ -39,9 +52,12 @@ def kFoldCross(trainCallback, predictCallback, X_dev, Y_dev, n_splits, err_func)
 
 
 def MeanAbsError(out: np.ndarray, target: np.ndarray):
+    """Mean absolute error"""
     return np.abs(out - target).mean()
 
 def BinaryAccuracy(out: np.ndarray, target: np.ndarray):
+    """Make `out` and `target` binary (x_bin = 1 if x>0, else 0)
+    and compute the error rate"""
     out = (out > 0).astype(int)
     target = (target > 0).astype(int)
     return np.abs(out - target).mean()
